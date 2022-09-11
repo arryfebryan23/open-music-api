220ba3af6799a0a9c7a833a53b32b2ce00348af3
arryfebryan23 <arryfebryan23@gmail.com>
2022-09-11 13:01:25 +0700

    create api service for albums

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
   const server = Hapi.server({
@@ -12,6 +16,16 @@ const init = async () => {
       },
     },
   });
+
+  await server.register(
+    {
+      plugin: albums,
+      options: {
+        service: new AlbumsService(),
+        validator: AlbumsValidator,
+      },
+    },
+  );
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
---
+++ src/validator/albums/schema.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const AlbumPayloadSchema = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.number().integer().min(1900).max(9999)
+    .required(),
+});
+
+module.exports = { AlbumPayloadSchema };
